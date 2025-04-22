89f727dabf02698b8902f4830fa4486cec862a80
Vinicius Araujo <vinicius.jesus49@etec.sp.gov.br>
2025-04-21 21:30:20 -0300

    subindo backend atualizado

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import publicRoutes from './routes/public.js';
-import privateRoutes from './routes/private.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use('/', publicRoutes);
-app.use('/', privateRoutes);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
+
+//git remote add origin https://github.com/Araujo2007/Sistema-Cardapio.git
+//git branch -M main
+//git push -u origin main
 
 //3 rotas: Criar pratos, separar por categoria, atualizar preço
 
